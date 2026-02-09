376b3ffbc8ef1fcf12fb2854acf21986356245fe
SamueLVitor1 <samuelvitor668@gmail.com>
2026-02-09 09:25:01 -0300

    feat: viewheader da content

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,13 +1,33 @@
-import { Image, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+import { Filter } from "@/components/Filter";
+import { FilterStatus } from "@/types/FilterStatus";
+
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require("@/assets/logo.png")} />
 
-      <Button title="Entrar" />
+      <View style={styles.form}>
+        <Input placeholder="O que você precisa comprar?" />
+        <Button title="Entrar" />
+      </View>
+
+      <View style={styles.content}>
+        <View style={styles.header}>
+          {FILTER_STATUS.map((status) => (
+            <Filter key={status} status={status} isActive />
+          ))}
+
+          <TouchableOpacity style={styles.clearButton}>
+            <Text style={styles.clearButtonText}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -3,16 +3,51 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
     backgroundColor: "#d0d2d8",
     paddingTop: 62,
-    padding: 24,
-
-    
   },
 
-  logo:{
+  logo: {
     height: 34,
-    width: 134
-  }
+    width: 134,
+  },
+
+  form: {
+    width: "100%",
+    paddingHorizontal: 16,
+    gap: 7,
+    marginTop: 42,
+  },
+
+  content: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "#fff",
+
+    borderTopRightRadius: 24,
+    borderTopLeftRadius: 24,
+
+    padding: 24,
+    paddingTop: 32,
+    marginTop: 24,
+  },
+
+  header: {
+    width: "100%",
+    flexDirection: "row",
+    gap: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: "#e4e6ec",
+    paddingBottom: 12
+  },
+
+  clearButton: {
+    marginLeft: "auto",
+  },
+  clearButtonText: {
+    fontSize: 12,
+    color: '#828282',
+    fontWeight: 600
+  },
 });
